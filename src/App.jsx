390fb1e76a0d18d4004ea77affd86f867084b422
kevinfeng <kevinfeng@fhnet.com.tw>
2026-02-12 09:51:02 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,48 @@
-import './App.css';
-import { HashRouter, NavLink } from 'react-router-dom';
+import "./App.css";
+import { HashRouter, NavLink, Routes, Route } from "react-router-dom";
+// import Login from "./pages";
+// import Register from "./pages";
+// import Todo from "./pages";
 
 const Todo = () => {
-  return <p>這是 Todo 頁面</p>;
+    return <p>這是 Todo 頁面</p>;
 };
 const Login = () => {
-  return <p>這是登入頁面</p>;
+    return <p>這是登入頁面</p>;
 };
 const Register = () => {
-  return <p>這是註冊頁面</p>;
+    return <p>這是註冊頁面</p>;
 };
 
 function App() {
-  return (
-    <div className="container">
-      <HashRouter>
-        <div className="nav-link">
-          <NavLink to="/">
-            <p>回到首頁</p>
-          </NavLink>
-          <NavLink to="/register">
-            <p>註冊頁面</p>
-          </NavLink>
-          <NavLink to="/login">
-            <p>登入頁面</p>
-          </NavLink>
-          <NavLink to="/todo">
-            <p>Todo 頁面</p>
-          </NavLink>
+    return (
+        <div className="container">
+            <HashRouter>
+                <div className="nav-link">
+                    <NavLink to="/">
+                        <p>回到首頁</p>
+                    </NavLink>
+                    <NavLink to="/register">
+                        <p>註冊頁面</p>
+                    </NavLink>
+                    <NavLink to="/login">
+                        <p>登入頁面</p>
+                    </NavLink>
+                    <NavLink to="/todo">
+                        <p>Todo 頁面</p>
+                    </NavLink>
+                </div>
+                {/* Routes, Route 練習區 */}
+                <Routes>
+                    <Route path="/" element={<h1>首頁</h1>}></Route>
+                    <Route path="/register" element={<Register />}></Route>
+                    <Route path="/login" element={<Login />}></Route>
+                    <Route path="/todo" element={<Todo />}></Route>
+                </Routes>
+                {/* 練習區 */}
+            </HashRouter>
         </div>
-        {/* Routes, Route 練習區 */}
-        {/* 練習區 */}
-      </HashRouter>
-    </div>
-  );
+    );
 }
 
 export default App;
